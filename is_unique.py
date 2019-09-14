63b2557b0ef2c7d9e6b834c2d86cd12bf8faef53
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-14 23:13:47 +0530

    Create is_unique.py

---
+++ is_unique.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+def is_unique(string):
+    """ Takes a string and returns True if it has all unique characters. """
+
+    str_set = set(string)
+
+    return str_set == string
+
+
+class Testing(unittest.TestCase):
+    def is_unique_test(self):
+        assertEqual(is_unique('asdfghjkl'), True)
+        assertEqual(is_unique('1234567asdf'), True)
+        assertEqual(is_unique('!@#$%^&asdfg123'), True)
+        assertEqual(is_unique('abcdABCD'), True)
+
+        assertEqual(is_unique('asdfghjkll'), False)
+        assertEqual(is_unique('1qwerty1'), False)
+        assertEqual(is_unique('poiu$asdf$'), False)
+
+if __name__ == '__main__':
+    unittest.main()
