5984e16bfeb3b13f03328d3a8c7b4b77a4875fc6
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-09-14 23:09:39 +0530

    Create count_recursively.py

---
+++ count_recursively.py
@@ -0,0 +1,22 @@
+def count_recursively(lst):
+    """ Return number of items in a list, using recursion.
+    >>> count_recursively([])
+    0
+
+    >>> count_recursively([1, 2, 3])
+    3
+    """
+
+
+    if not lst:
+        return 0
+
+    return 1 + count_recursively(lst[1:])
+
+
+if __name__ == '__main__':
+    import doctest
+    results = doctest.testmod()
+
+    if results.failed == 0:
+        print "ALL TESTS PASSED!"
